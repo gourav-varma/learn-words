74d0d734e0a4aa76864e34cc47c44a4a675d40d4
Gourav Varma <gourav@DESKTOP-KNS1ECB.localdomain>
2023-02-07 23:07:32 +0530

    added tailwind

---
+++ src/Homepage.js
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react";
+
+const Homepage = () => {
+  return (
+    <h1 className="text-5xl font-bold underline">
+		Hi folksss
+    </h1>
+  )
+}
+
+export default Homepage
